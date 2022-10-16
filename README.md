a20ec3f336d3625b699988e6bf79a6d60f93aeed
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2022-10-16 15:46:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hugin Node.JS Client
+This is a basic hugin messenger client written in Node.JS using our newly published POST in hugin API
+
+# USAGE
+Go through the client.js file and do the following:
+1) Add your address + the private key of said address
+2) Add the address of a updated hugin API, for example the official one (https://api.hugin.chat)
---
+++ client.js
@@ -0,0 +1,136 @@
+const KryptokronaUtils = require('kryptokrona-utils')
+const cnUtil = new KryptokronaUtils.CryptoNote()
+const nacl = require('tweetnacl')
+const naclUtil = require('tweetnacl-util')
+const prompt = require('prompt-sync')();
+var cron = require('node-cron');
+
+function toHex(str,hex) {
+    try {
+        hex = unescape(encodeURIComponent(str))
+        .split('').map(function(v){
+            return v.charCodeAt(0).toString(16)
+        }).join('')
+    } catch(e) {
+        hex = str
+    }
+    
+    return hex
+}
+
+function nonceFromTimestamp(tmstmp) {
+    let nonce = hexToUint(String(tmstmp));
+    
+    while (nonce.length < nacl.box.nonceLength) {
+        tmp_nonce = Array.from(nonce)
+        tmp_nonce.push(0);
+        nonce = Uint8Array.from(tmp_nonce)
+    }
+    
+    return nonce;
+}
+
+function hexToUint(hexString) {
+    return new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)))
+}
+
+async function sendGroupsMessage(message, nickname, group) {    
+    const my_address = "SET YOUR ADDRESS"
+    const private_key = 'SET THE PRIVATE KEY FOR ADDRESS' 
+    const signature = await cnUtil.signMessage(message, private_key)
+    const timestamp = parseInt(Date.now())
+    const nonce = nonceFromTimestamp(timestamp)
+    
+    
+    let message_json = {
+        "m": message,
+        "k": my_address,
+        "s": signature,
+        "g": group,
+        "n": nickname
+    }
+    
+    const payload_unencrypted = naclUtil.decodeUTF8(JSON.stringify(message_json))
+    const secretbox = nacl.secretbox(payload_unencrypted, nonce, hexToUint(group))
+    const payload_encrypted = {"sb":Buffer.from(secretbox).toString('hex'), "t":timestamp}
+    const payload_encrypted_hex = toHex(JSON.stringify(payload_encrypted))
+    
+    return await fetch(`CHOOSE.AN.API/api/v1/posts`, {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ payload: payload_encrypted_hex }),
+}).then((response) => {
+    return response.json()
+}).then((json) => {
+    // console.log(json)
+})
+}
+
+async function getGroupPosts(possibleKey) {
+    fetch('CHOOSE.AN.API/api/v1/posts-encrypted-group?size=3')
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        // console.log(json.encrypted_group_posts)
+        console.log("___________________ LOADING THE 3 LAST MESSAGES ___________________ ")
+        json.encrypted_group_posts.forEach(encrypted_group_posts => {
+            // console.log("sig: " + hexToUint(possibleKey))
+            let decryptBox = false;
+            let key;
+            let groups = [possibleKey];
+            let i = 0;
+            while (!decryptBox && i < groups.length) {
+                i += 1;
+                try { 
+                    decryptBox = nacl.secretbox.open(
+                        hexToUint(encrypted_group_posts.tx_sb),
+                        nonceFromTimestamp(encrypted_group_posts.tx_timestamp),
+                        hexToUint(possibleKey)
+                        );        
+                        key = possibleKey;
+                    } catch (err) {
+                        console.log(err);
+                        continue;
+                    }
+                }
+                if (!decryptBox) {
+                    return false;
+                }
+                const message_dec = naclUtil.encodeUTF8(decryptBox);
+                const payload_json = JSON.parse(message_dec);
+                const nickname = payload_json.n ? payload_json.n : t('Anonymous');
+                // console.log(payload_json)
+                console.log(nickname + ": " + payload_json.m)
+                return
+            })
+        })
+    }
+    async function sendMessage() {
+        username = prompt("Whats your username? ")
+        group = prompt("What group do you want to send to? ")
+        message = prompt("What message do you want to send? ")
+        choice = prompt("Are you sure that you want to send '" + message + "' in the name of '" + username + "' to '" + group + "'? yes/no ")
+        if(choice == "yes") {
+            sendGroupsMessage(message, username, group)
+            getGroupPosts(group)
+        } else {
+            return
+        }
+    }
+    
+    async function main() {
+        console.log("Welcome to the Node.js Hugin Client")
+        sendMessage()
+        cron.schedule('*/1 * * * *', () => {
+            getGroupPosts(group)
+        });
+        
+    }
+    main()
+    
+    
+    
+    
